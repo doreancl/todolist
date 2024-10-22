5b871ee082518df14d8bccabea91ed33f7e1d61f
doreancl <doreancl@gmail.com>
2024-10-22 16:13:17 -0300

    Remix Tutorial 30 min

--- app/root.tsx
+++ app/root.tsx
@@ -1,45 +1,153 @@
+import type {LinksFunction, LoaderFunctionArgs} from "@remix-run/node";
+import {json, redirect} from "@remix-run/node";
 import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
+    Form,
+    Link,
+    Links,
+    Meta,
+    NavLink,
+    Outlet,
+    Scripts,
+    ScrollRestoration,
+    useLoaderData,
+    useNavigation,
+    useSubmit
 } from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
-
-import "./tailwind.css";
+// existing imports
+import appStylesHref from "./app.css?url";
+import {createEmptyContact, getContacts} from "./data";
+import {useEffect} from "react";
 
 export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
+    {rel: "stylesheet", href: appStylesHref},
 ];
 
-export function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
-}
+export const loader = async ({
+                                 request,
+                             }: LoaderFunctionArgs) => {
+    const url = new URL(request.url);
+    const q = url.searchParams.get("q");
+    const contacts = await getContacts(q);
+    return json({contacts, q});
+};
+
+export const action = async () => {
+    const contact = await createEmptyContact();
+    return redirect(`/contacts/${contact.id}/edit`);
+};
 
 export default function App() {
-  return <Outlet />;
+    const {contacts, q} = useLoaderData<typeof loader>();
+    const navigation = useNavigation();
+    const submit = useSubmit();
+    const searching =
+        navigation.location &&
+        new URLSearchParams(navigation.location.search).has(
+            "q"
+        );
+
+    useEffect(() => {
+        const searchField = document.getElementById("q");
+        if (searchField instanceof HTMLInputElement) {
+            searchField.value = q || "";
+        }
+    }, [q]);
+
+    return (
+        <html lang="en">
+        <head>
+            <meta charSet="utf-8"/>
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1"
+            />
+            <Meta/>
+            <Links/>
+        </head>
+        <body>
+        <div id="sidebar">
+            <h1>
+                <Link to={`/`}>
+                    Remix Contacts
+                </Link>
+            </h1>
+            <div>
+                <Form id="search-form" role="search"
+                      onChange={(event) => {
+                          const isFirstSearch = q === null;
+                          submit(event.currentTarget, {
+                              replace: !isFirstSearch,
+                          });
+                      }}
+
+                >
+                    <input
+                        aria-label="Search contacts"
+                        id="q"
+                        name="q"
+                        placeholder="Search"
+                        type="search"
+                        defaultValue={q || ""}
+                        className={searching ? "loading" : ""}
+                    />
+                    <div
+                        aria-hidden
+                        id="search-spinner"
+                        hidden={!searching}
+                    />
+                </Form>
+                <Form method="post">
+                    <button type="submit">New</button>
+                </Form>
+            </div>
+            <nav>
+                {contacts.length ? (
+                    <ul>
+                        {contacts.map((contact) => (
+                            <li key={contact.id}>
+
+                                <NavLink
+                                    className={({isActive, isPending}) =>
+                                        isActive
+                                            ? "active"
+                                            : isPending
+                                                ? "pending"
+                                                : ""
+                                    }
+                                    to={`contacts/${contact.id}`}
+                                >
+                                    <Link to={`contacts/${contact.id}`}>
+                                        {contact.first || contact.last ? (
+                                            <>
+                                                {contact.first} {contact.last}
+                                            </>
+                                        ) : (
+                                            <i>No Name</i>
+                                        )}{" "}
+                                        {contact.favorite ? (
+                                            <span>★</span>
+                                        ) : null}
+                                    </Link>
+                                </NavLink>
+
+                            </li>
+                        ))}
+                    </ul>
+                ) : (
+                    <p>
+                        <i>No contacts</i>
+                    </p>
+                )}
+            </nav>
+        </div>
+        <div id="detail" className={
+            navigation.state === "loading" && !searching ? "loading" : "ass"
+        }>
+            <Outlet/>
+        </div>
+        <ScrollRestoration/>
+        <Scripts/>
+        </body>
+        </html>
+    );
 }
